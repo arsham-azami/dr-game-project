dbb5c035161df056c54aba5cb5b428e542ff0b4b
arsham azami <arsham2020azami@example.com>
2020-06-23 13:34:37 +0430

    commiit it

--- src/App.js
+++ src/App.js
@@ -24,18 +24,19 @@ export default function App() {
       </div>
 
       <div className ={'body'}>
-        <h3>Game club for dr game</h3>
+        <h3>Game club for dr.game</h3>
          <h3> assassins creed unity</h3>
          <h3> assassins creed origins</h3>
          <h3> assassins creed oddessey</h3>
        </div>
       
        <div className={'footer'}>
-          <h3>tweeter</h3> 
-          <h3>instagram</h3> 
-          <h3>facebook</h3> 
-          <h3>telegram</h3> 
-          <h3>pinterest</h3> 
+          <h3>Tweeter</h3> 
+          <h3>Instagram</h3> 
+          <h3>Facebook</h3> 
+          <h3>Telegram</h3> 
+          <h3>Github</h3> 
+          <h3>Pinterest</h3> 
        </div>
      
     </div>
