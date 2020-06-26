32efd7cf307a7eace4224ac55772930edf80ed21
arsham azami <arsham2020azami@example.com>
2020-06-26 13:58:56 +0430

    alot changes

--- src/App.js
+++ src/App.js
@@ -5,32 +5,56 @@ import Product from './component/Product'
 
 export default function App() {
 
+  if(document.innerHeight > 70){
+    const ele = document.createElement('p')
+    ele.innerText='top'
+  }
+
   return (
    <>
-    <div className={'grid-container'}>
+    <div className={'grid-container'} id='main'>
 
       <div className={'header'}>
     
-       <h2><a href='/'>Dr.game</a></h2>
+       <h1><a href='/'>Dr.game</a></h1>
         <ul>
           <li><a href='#'>Home</a></li> 
           <li><a href='#'>Steam</a></li> 
           <li><a href='#'>Products</a></li> 
           <li><a href='#'>About us</a></li> 
         </ul>
-        <i class="fa fa-shopping-cart">chart</i>
+        
         <div className={'btn-group'}>
           <button className={'btn btn-success'}>Sign in</button>
           <button className={'btn btn-success'}>Sign up</button>
         </div>
+
       </div>
 
       <div className ={'body'}>
-        <div className={'product-container'}>
-            <Product name={'ac origins'} price={'$120'} rate={'4.5'} alt={'acor'} src={require('public/images/logo')}></Product>
-            <Product name={'ac oddessey'} price={'$170'} rate={'4.5'} alt={'acod'} src={'public/images/logo 2.jpeg'}></Product>
+
+      <p>News</p>
+        <div className={'game-news'}>
+            <div className={'news-one'}></div>
+            <div className={'news-two'}></div>
+            <div className={'news-three'}></div>
+            <div className={'news-four'}></div>
+        </div>
+          
+        <p className={'title-one'}>Best seller</p> 
+        <div className={'product-container-one'}>
+            <Product name={'ac origins'} price={'$120'} rate={4.5} alt={'acor'} ></Product>
+            <Product name={'ac oddessey'} price={'$170'} rate={4.5} alt={'acod'}></Product>
             <Product name={'red dead'} price={'$100'} rate={4} alt={'rdr'}></Product>
-       </div>
+         </div>
+         
+         <p className={'title-two'}>sell</p>
+         <div className={'product-container-two'}>
+            <Product name={'battlefield'} price={'$100'} rate={4} alt={'rdr'}></Product>
+            <Product name={'call of duty'} price={'$100'} rate={4} alt={'rdr'}></Product>
+            <Product name={'need for speed'} price={'$120'} rate={5} alt={'nfs'}></Product>
+        </div>
+        
      </div>
 
        <div className={'footer'}>
@@ -40,8 +64,9 @@ export default function App() {
           <h3>Telegram</h3> 
           <h3>Github</h3> 
           <h3>Pinterest</h3> 
+
        </div>
-     
+                        
     </div>
     </>
    )
--- src/component/product.js
+++ src/component/product.js
@@ -1,15 +1,48 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+// import {UsesState} from 'react'
 export default function Product(props){
+  
+    let[numchar, setnumchar]=useState(0)
+
+    const addinToChart =()=>{
+        alert('added to chart')
+     }
+
+    const incraesing =()=>{
+        setnumchar(numchar=numchar+1)
+    }
+    
+    const decreasing =()=>{
+        setnumchar(numchar=numchar-1)
+        if(numchar < 0){
+            setnumchar(numchar=0)
+        }
+}
     return(
-        <div className={'product'}>
-           <img src={props.src} alt={props.alt}></img>
+
+        <div  className={'product'}>
+          <div className={'add-element'}>
+                <div className={'pro-image'} src={props.src} alt={props.alt}></div>
+                    <div className={'like-button'}>         
+                        <button>Like</button>
+                        <button>Unlike</button>
+                        <button>Comments</button>
+                    </div> 
+                </div>
             <div className={'info'}>
-                <h3>Name:{props.name}</h3>    
+                <p className={'pro-title'}>{props.name}</p>    
                 <h3>Price:{props.price}</h3>    
                 <h3>Rating:{props.rate}</h3>  
             </div> 
-            <button className={'add-to-chart'}>Add to chart</button>  
+            
+            <div className={'num-button'}>
+               <button onClick={incraesing}>+</button>
+                   <span>{numchar}</span>
+               <button onClick={decreasing}>-</button>
+            </div>
+
+            <button className={'add-to-chart'} onClick={addinToChart}>Add to chart</button>  
         </div>
+    
     )
 }
